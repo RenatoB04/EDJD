715ca4b584fa5daea3bce8f221d4a7f2f37fcacd
RenatoB04 <renatob04@icloud.com>
2024-03-13 23:07:54 +0000

    Added remove row/column option. Fixed code format

--- main.c
+++ main.c
@@ -1,230 +1,305 @@
-#include <stdio.h>
 #include <stdbool.h>
+#include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 
 typedef struct Node {
-    int data;
-    struct Node* next;
+  int data;
+  struct Node* next;
 } Node;
 
 typedef struct ED {
-    Node* head;
+  Node* head;
 } ED;
 
 int maxSum = 0;
-int *combination;
-int *maxCombination;
-bool *usedIndices;
+int* combination;
+int* maxCombination;
+bool* usedIndices;
 
-void findMaxSum(ED* matrix, int n, int m, int row, int col, int sum, bool *usedRows, bool *usedCols) {
-    if (row == n) {
-        if (sum > maxSum) {
-            maxSum = sum;
-            for (int i = 0; i < n; i++) {
-                if (usedRows[i]) {
-                    maxCombination[i] = combination[i];
-                    usedIndices[i] = true;
-                } else {
-                    usedIndices[i] = false;
-                }
-            }
+void findMaxSum(ED* matrix, int n, int m, int row, int col, int sum,
+                bool* usedRows, bool* usedCols) {
+  if (row == n) {
+    if (sum > maxSum) {
+      maxSum = sum;
+      for (int i = 0; i < n; i++) {
+        if (usedRows[i]) {
+          maxCombination[i] = combination[i];
+          usedIndices[i] = true;
+        } else {
+          usedIndices[i] = false;
         }
-        return;
+      }
     }
+    return;
+  }
 
-    Node* node = matrix[row].head;
-    for (int j = 0; j < m; j++) {
-        if (!usedCols[j] && !usedRows[row]) {
-            combination[row] = node->data;
-            usedCols[j] = true;
-            usedRows[row] = true;
-            findMaxSum(matrix, n, m, row + 1, 0, sum + node->data, usedRows, usedCols);
-            usedCols[j] = false;
-            usedRows[row] = false;
-        }
-        node = node->next;
+  Node* node = matrix[row].head;
+  for (int j = 0; j < m; j++) {
+    if (!usedCols[j] && !usedRows[row]) {
+      combination[row] = node->data;
+      usedCols[j] = true;
+      usedRows[row] = true;
+      findMaxSum(matrix, n, m, row + 1, 0, sum + node->data, usedRows,
+                 usedCols);
+      usedCols[j] = false;
+      usedRows[row] = false;
     }
+    node = node->next;
+  }
 
-    if (node == NULL) {
-        findMaxSum(matrix, n, m, row + 1, 0, sum, usedRows, usedCols);
-    }
+  if (node == NULL) {
+    findMaxSum(matrix, n, m, row + 1, 0, sum, usedRows, usedCols);
+  }
 }
 
 void insertNode(ED* matrix, int data) {
-    Node* newNode = malloc(sizeof(Node));
-    newNode->data = data;
-    newNode->next = NULL;
+  Node* newNode = malloc(sizeof(Node));
+  newNode->data = data;
+  newNode->next = NULL;
 
-    if (matrix->head == NULL) {
-        matrix->head = newNode;
-    } else {
-        Node* temp = matrix->head;
-        while (temp->next != NULL) {
-            temp = temp->next;
-        }
-        temp->next = newNode;
+  if (matrix->head == NULL) {
+    matrix->head = newNode;
+  } else {
+    Node* temp = matrix->head;
+    while (temp->next != NULL) {
+      temp = temp->next;
     }
+    temp->next = newNode;
+  }
 }
 
 void insertColumn(ED* matrix, int row, int data) {
-    Node* newNode = malloc(sizeof(Node));
-    newNode->data = data;
-    newNode->next = NULL;
+  Node* newNode = malloc(sizeof(Node));
+  newNode->data = data;
+  newNode->next = NULL;
 
-    if (matrix[row].head == NULL) {
-        matrix[row].head = newNode;
-    } else {
-        Node* temp = matrix[row].head;
-        while (temp->next != NULL) {
-            temp = temp->next;
-        }
-        temp->next = newNode;
+  if (matrix[row].head == NULL) {
+    matrix[row].head = newNode;
+  } else {
+    Node* temp = matrix[row].head;
+    while (temp->next != NULL) {
+      temp = temp->next;
     }
+    temp->next = newNode;
+  }
 }
 
 void printMatrix(ED* matrix, int n, int m) {
-    for (int i = 0; i < n; i++) {
-        Node* node = matrix[i].head;
-        while (node != NULL) {
-            printf("%5d ", node->data);
-            node = node->next;
-        }
-        printf("\n");
+  system("cls");
+  printf("Matriz:\n");
+  for (int i = 0; i < n; i++) {
+    Node* node = matrix[i].head;
+    while (node != NULL) {
+      printf("%5d ", node->data);
+      node = node->next;
     }
+    printf("\n");
+  }
 }
 
 void printMaxCombination(ED* matrix, int n, int m) {
-    printf("\nElementos selecionados:\n");
-    for (int i = 0; i < n; i++) {
-        if (usedIndices[i]) {
-            printf("%d ", maxCombination[i]);
-        }
+  printf("\nElementos selecionados:\n");
+  for (int i = 0; i < n; i++) {
+    if (usedIndices[i]) {
+      printf("%d ", maxCombination[i]);
     }
-    printf("\n");
+  }
+  printf("\n");
+}
+
+void removeRow(ED** matrix, int* n, int row) {
+  Node* node = (*matrix)[row].head;
+  while (node != NULL) {
+    Node* next = node->next;
+    free(node);
+    node = next;
+  }
+
+  for (int i = row; i < *n - 1; i++) {
+    (*matrix)[i] = (*matrix)[i + 1];
+  }
+
+  *matrix = realloc(*matrix, (*n - 1) * sizeof(ED));
+  (*n)--;
+}
+
+void removeColumn(ED* matrix, int n, int* m, int col) {
+  for (int i = 0; i < n; i++) {
+    Node* node = matrix[i].head;
+    Node* prev = NULL;
+    for (int j = 0; j < col; j++) {
+      prev = node;
+      node = node->next;
+    }
+
+    if (prev == NULL) {
+      matrix[i].head = node->next;
+    } else {
+      prev->next = node->next;
+    }
+
+    free(node);
+  }
+
+  (*m)--;
 }
 
 void freeMatrix(ED* matrix, int n) {
-    for (int i = 0; i < n; i++) {
-        Node* node = matrix[i].head;
-        while (node != NULL) {
-            Node* next = node->next;
-            free(node);
-            node = next;
-        }
+  for (int i = 0; i < n; i++) {
+    Node* node = matrix[i].head;
+    while (node != NULL) {
+      Node* next = node->next;
+      free(node);
+      node = next;
     }
-    free(matrix);
+  }
+  free(matrix);
 }
 
 int main() {
-    FILE *file = fopen("matriz.txt", "r");
-    if (file == NULL) {
-        fprintf(stderr, "Erro ao abrir o arquivo.\n");
+  FILE* file = fopen("matriz.txt", "r");
+  if (file == NULL) {
+    fprintf(stderr, "Erro ao abrir o arquivo.\n");
+    return 1;
+  }
+
+  int n = 0, m = 0;
+  char ch;
+  while (!feof(file)) {
+    ch = fgetc(file);
+    if (ch == '\n' || ch == EOF) {
+      n++;
+    }
+    if (ch == ';' && n == 1) {
+      m++;
+    }
+  }
+  m++;
+  rewind(file);
+
+  ED* matrix = malloc(n * sizeof(ED));
+  for (int i = 0; i < n; i++) {
+    matrix[i].head = NULL;
+    Node** current = &(matrix[i].head);
+    for (int j = 0; j < m; j++) {
+      *current = malloc(sizeof(Node));
+      if (fscanf(file, "%d;", &((*current)->data)) != 1) {
+        fprintf(stderr, "Erro ao ler o arquivo.\n");
+        fclose(file);
+        freeMatrix(matrix, n);
         return 1;
+      }
+      current = &((*current)->next);
     }
+    *current = NULL;
+  }
 
-    int n = 0, m = 0;
-    char ch;
-    while(!feof(file)) {
-        ch = fgetc(file);
-        if(ch == '\n' || ch == EOF) {
-            n++;
+  fclose(file);
+
+  combination = malloc(n * sizeof(int));
+  maxCombination = malloc(n * sizeof(int));
+  usedIndices = calloc(n, sizeof(bool));
+
+  int option;
+  do {
+    printf("\nOpcoes:\n");
+    printf("1. Calcular matriz\n");
+    printf("2. Adicionar linha/coluna\n");
+    printf("3. Remover linha/coluna\n");
+    printf("4. Sair\n");
+    printf("Opcao: ");
+    scanf("%d", &option);
+    getchar();
+
+    switch (option) {
+      case 1:
+        printMatrix(matrix, n, m);
+        maxSum = 0;
+        memset(maxCombination, 0, n * sizeof(int));
+        bool* usedRows = calloc(n, sizeof(bool));
+        bool* usedCols = calloc(m, sizeof(bool));
+        findMaxSum(matrix, n, m, 0, 0, 0, usedRows, usedCols);
+        printMaxCombination(matrix, n, m);
+        printf("\nMaior soma possivel: %d\n", maxSum);
+        free(usedRows);
+        free(usedCols);
+        break;
+      case 2:
+        printf("\nAdicionar uma linha (1) ou uma coluna (2)?\n");
+        int addOption;
+        scanf("%d", &addOption);
+        getchar();
+        if (addOption == 1) {
+          printf("\nInsira os numeros para a nova linha no formato 1;2;3;n:\n");
+          ED* newLineMatrix = malloc(sizeof(ED));
+          newLineMatrix->head = NULL;
+          for (int j = 0; j < m; j++) {
+            int data;
+            scanf("%d;", &data);
+            insertNode(newLineMatrix, data);
+          }
+          matrix = realloc(matrix, (n + 1) * sizeof(ED));
+          matrix[n] = *newLineMatrix;
+          n++;
+          free(newLineMatrix);
+        } else if (addOption == 2) {
+          printf(
+              "\nInsira os numeros para a nova coluna no formato 1;2;3;n:\n");
+          for (int i = 0; i < n; i++) {
+            int data;
+            scanf("%d;", &data);
+            insertColumn(matrix, i, data);
+          }
+          m++;
+        } else {
+          printf("\nOpcao invalida.\n");
         }
-        if(ch == ';' && n == 1) {
-            m++;
+        break;
+      case 3:
+        printf("\nRemover uma linha (1) ou uma coluna (2)?\n");
+        int removeOption;
+        scanf("%d", &removeOption);
+        getchar();
+        if (removeOption == 1) {
+          printf("\nMatriz atual:\n");
+          printMatrix(matrix, n, m);
+          printf("\nInsira o numero da linha para remover (1 a %d):\n", n);
+          int row;
+          scanf("%d", &row);
+          getchar();
+          if (row >= 1 && row <= n) {
+            removeRow(&matrix, &n, row - 1);
+          } else {
+            printf("\nNumero de linha invalido.\n");
+          }
+        } else if (removeOption == 2) {
+          printf("\nMatriz atual:\n");
+          printMatrix(matrix, n, m);
+          printf("\nInsira o numero da coluna para remover (1 a %d):\n", m);
+          int col;
+          scanf("%d", &col);
+          getchar();
+          if (col >= 1 && col <= m) {
+            removeColumn(matrix, n, &m, col - 1);
+          } else {
+            printf("\nNumero de coluna invalido.\n");
+          }
+        } else {
+          printf("\nOpcao invalida.\n");
         }
+        break;
+      case 4:
+        break;
+      default:
+        printf("\nOpcao invalida.\n");
     }
-    m++;
-    rewind(file);
+  } while (option != 4);
 
-    ED* matrix = malloc(n * sizeof(ED));
-    for (int i = 0; i < n; i++) {
-        matrix[i].head = NULL;
-        Node** current = &(matrix[i].head);
-        for (int j = 0; j < m; j++) {
-            *current = malloc(sizeof(Node));
-            if (fscanf(file, "%d;", &((*current)->data)) != 1) {
-                fprintf(stderr, "Erro ao ler o arquivo.\n");
-                fclose(file);
-                freeMatrix(matrix, n);
-                return 1;
-            }
-            current = &((*current)->next);
-        }
-        *current = NULL;
-    }
+  freeMatrix(matrix, n);
+  free(combination);
+  free(maxCombination);
+  free(usedIndices);
 
-    fclose(file);
-
-    combination = malloc(n * sizeof(int));
-    maxCombination = malloc(n * sizeof(int));
-    usedIndices = calloc(n, sizeof(bool));
-
-    int option;
-        do {
-            printf("\nOpcoes:\n");
-            printf("1. Calcular matriz\n");
-            printf("2. Adicionar linha/coluna\n");
-            printf("3. Sair\n");
-            printf("Opcao: ");
-            scanf("%d", &option);
-            getchar();
-    
-            switch(option) {
-                case 1:
-                    printf("\nMatriz:\n");
-                    printMatrix(matrix, n, m);
-                    maxSum = 0;
-                    memset(maxCombination, 0, n * sizeof(int));
-                    bool *usedRows = calloc(n, sizeof(bool));
-                    bool *usedCols = calloc(m, sizeof(bool));
-                    findMaxSum(matrix, n, m, 0, 0, 0, usedRows, usedCols);
-                    printMaxCombination(matrix, n, m);
-                    printf("\nMaior soma possivel: %d\n", maxSum);
-                    free(usedRows);
-                    free(usedCols);
-                    break;
-            case 2:
-                            printf("\nAdicionar uma linha (0) ou uma coluna (1)?\n");
-                            int addOption;
-                            scanf("%d", &addOption);
-                            getchar();       
-                            if (addOption == 0) {
-                                printf("\nInsira os numeros para a nova linha no formato 1;2;3;n:\n");
-                                ED* newLineMatrix = malloc(sizeof(ED));
-                                newLineMatrix->head = NULL;
-                                for (int j = 0; j < m; j++) {
-                                    int data;
-                                    scanf("%d;", &data);
-                                    insertNode(newLineMatrix, data);
-                                }
-                                matrix = realloc(matrix, (n + 1) * sizeof(ED));
-                                matrix[n] = *newLineMatrix;
-                                n++;
-                                free(newLineMatrix);
-                            } else if (addOption == 1) {
-                                printf("\nInsira os numeros para a nova coluna no formato 1;2;3;n:\n");
-                                for (int i = 0; i < n; i++) {
-                                    int data;
-                                    scanf("%d;", &data);
-                                    insertColumn(matrix, i, data);
-                                }
-                                m++;
-                            } else {
-                                printf("\nOpcao invalida.\n");
-                            }
-                            break;
-            case 3:
-                break;
-            default:
-                printf("\nOpcao invalida.\n");
-        }
-    } while (option != 3);
-
-    freeMatrix(matrix, n);
-    free(combination);
-    free(maxCombination);
-    free(usedIndices);
-
-    return 0;
+  return 0;
 }
