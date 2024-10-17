0344d12a6e88b358166aa794d2bb3272944a3559
RenatoB04 <renatob04@icloud.com>
2024-10-17 20:16:40 +0100

    Add dynamic resizing for news titles and adjust button size accordingly

--- app/src/main/java/com/examples/e02_djpm/ui/RowArticle.kt
+++ app/src/main/java/com/examples/e02_djpm/ui/RowArticle.kt
@@ -1,10 +1,13 @@
 package com.examples.e02_djpm.ui
 
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.shadow
 import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -14,22 +17,23 @@ import java.util.Date
 
 @Composable
 fun RowArticle(modifier: Modifier = Modifier, article: Article) {
-    Row(
+    Surface(
         modifier = modifier
             .fillMaxWidth()
-            .padding(12.dp)
+            .padding(8.dp)
+            .shadow(4.dp, RoundedCornerShape(8.dp)),
+        color = MaterialTheme.colorScheme.background,
+        shape = RoundedCornerShape(8.dp)
     ) {
-        Column(modifier = Modifier.fillMaxWidth()) {
+        Column(modifier = Modifier
+            .fillMaxWidth()
+            .padding(16.dp)) {
 
             Text(
                 text = article.title ?: "No Title Available",
                 style = MaterialTheme.typography.titleMedium,
-                maxLines = 2,
-                overflow = TextOverflow.Ellipsis,
-                modifier = Modifier.padding(bottom = 8.dp)
+                overflow = TextOverflow.Visible
             )
-
-            Spacer(modifier = Modifier.height(8.dp))
         }
     }
 }
@@ -40,7 +44,7 @@ fun RowArticlePreview() {
     E02DJPMTheme {
         RowArticle(
             article = Article(
-                "Sample Title",
+                "Titulo de exemplo. Este é um título que deverá ajustar dinamicamente o tamanho do botão para fazer com que caiba o título inteiro.",
                 null,
                 null,
                 "url",
