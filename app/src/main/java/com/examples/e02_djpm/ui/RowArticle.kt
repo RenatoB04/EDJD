78b129c416fcead7856659305f05bed2f633b8c2
RenatoB04 <renatob04@icloud.com>
2024-10-22 17:32:07 +0100

    Adjusted RowArticle.kt to include images and description

--- app/src/main/java/com/examples/e02_djpm/ui/RowArticle.kt
+++ app/src/main/java/com/examples/e02_djpm/ui/RowArticle.kt
@@ -1,39 +1,74 @@
 package com.examples.e02_djpm.ui
 
-import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.draw.shadow
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import coil.compose.AsyncImage
+import com.examples.e02_djpm.R
 import com.examples.e02_djpm.models.Article
+import com.examples.e02_djpm.toYYYYMMDD
 import com.examples.e02_djpm.ui.theme.E02DJPMTheme
 import java.util.Date
 
 @Composable
 fun RowArticle(modifier: Modifier = Modifier, article: Article) {
-    Surface(
-        modifier = modifier
-            .fillMaxWidth()
-            .padding(8.dp)
-            .shadow(4.dp, RoundedCornerShape(8.dp)),
-        color = MaterialTheme.colorScheme.background,
-        shape = RoundedCornerShape(8.dp)
-    ) {
+    Row(modifier = modifier) {
+        article.urlToImage?.let {
+            AsyncImage(
+                model = it,
+                contentDescription = "image article",
+                modifier = Modifier
+                    .height(120.dp)
+                    .width(120.dp)
+                    .clip(RoundedCornerShape(8.dp))
+                    .padding(6.dp)
+                    .shadow(4.dp, RoundedCornerShape(8.dp)),
+                contentScale = ContentScale.Crop
+            )
+        } ?: run {
+            Image(
+                modifier = Modifier
+                    .height(120.dp)
+                    .width(120.dp)
+                    .clip(RoundedCornerShape(8.dp))
+                    .padding(6.dp)
+                    .shadow(4.dp, RoundedCornerShape(8.dp)),
+                painter = painterResource(id = R.mipmap.img_place_holder),
+                contentDescription = "image article",
+                contentScale = ContentScale.Crop,
+            )
+        }
         Column(modifier = Modifier
             .fillMaxWidth()
-            .padding(16.dp)) {
-
+            .padding(start = 8.dp)) {
             Text(
-                text = article.title ?: "No Title Available",
-                style = MaterialTheme.typography.titleMedium,
-                overflow = TextOverflow.Visible
+                text = article.title ?: "",
+                style = MaterialTheme.typography.titleLarge,
+                maxLines = 2,
+                overflow = TextOverflow.Ellipsis
             )
+            Text(
+                text = article.description ?: "",
+                maxLines = 3,
+                overflow = TextOverflow.Ellipsis
+            )
+            Text(text = article.publishedAt?.toYYYYMMDD() ?: "")
         }
     }
 }
@@ -42,14 +77,12 @@ fun RowArticle(modifier: Modifier = Modifier, article: Article) {
 @Composable
 fun RowArticlePreview() {
     E02DJPMTheme {
-        RowArticle(
-            article = Article(
-                "Titulo de exemplo. Este é um título que deverá ajustar dinamicamente o tamanho do botão para fazer com que caiba o título inteiro.",
-                null,
-                null,
-                "url",
-                Date()
-            )
-        )
+        RowArticle(article = Article(
+            "Title",
+            "description",
+            null,
+            "url",
+            Date()
+        ))
     }
 }
